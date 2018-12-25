0587ccedf2d72300918ca37ce016d95a7bde1c61
Eren Shi <eren.shi@nextlabs.com>
2018-12-25 18:54:07 +0800

    first commit

---
+++ SKIncomeTaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKIncomeTaxCalculator
+//
+//  Created by Eren on 2018/12/25.
+//  Copyright © 2018 skyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKIncomeTaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKIncomeTaxCalculator
+//
+//  Created by Eren on 2018/12/25.
+//  Copyright © 2018 skyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
